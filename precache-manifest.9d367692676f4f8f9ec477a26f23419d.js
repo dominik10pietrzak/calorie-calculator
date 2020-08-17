984b6508b198630d1e01dd77f6ee7bbb79432b9e
dominik10pietrzak <d.pietrzak516@op.pl>
2020-08-17 16:49:35 +0200

    Updates

---
+++ precache-manifest.9d367692676f4f8f9ec477a26f23419d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4989ff1cc45cba32a6e82157640a8a7",
+    "url": "/calorie-calculator/index.html"
+  },
+  {
+    "revision": "d96fe2b40e99dc9662fd",
+    "url": "/calorie-calculator/static/css/main.a5e9ea89.chunk.css"
+  },
+  {
+    "revision": "479f9c95d7a57011c087",
+    "url": "/calorie-calculator/static/js/2.75b28119.chunk.js"
+  },
+  {
+    "revision": "24977d6f8d5e08c25008a19904137a11",
+    "url": "/calorie-calculator/static/js/2.75b28119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d96fe2b40e99dc9662fd",
+    "url": "/calorie-calculator/static/js/main.bd1b4670.chunk.js"
+  },
+  {
+    "revision": "c3eeaf28cb4b078e5a7c",
+    "url": "/calorie-calculator/static/js/runtime-main.79ea2b43.js"
+  },
+  {
+    "revision": "5f56175e7b0b0201494d5124c0712b62",
+    "url": "/calorie-calculator/static/media/bg-main-mobile.5f56175e.jpg"
+  },
+  {
+    "revision": "a4c1e68536f1e1819ceea0553bd23a71",
+    "url": "/calorie-calculator/static/media/bg-main-tab.a4c1e685.jpg"
+  },
+  {
+    "revision": "b289be06cb0b08824667e84b7b52f4b1",
+    "url": "/calorie-calculator/static/media/bg-main.b289be06.jpg"
+  },
+  {
+    "revision": "7bcc2369fe43b7bf3a9ffd3a2528ae38",
+    "url": "/calorie-calculator/static/media/bg-score.7bcc2369.jpg"
+  },
+  {
+    "revision": "ca4e14fd62461561af6154a816ad278e",
+    "url": "/calorie-calculator/static/media/deadlift.ca4e14fd.jpg"
+  },
+  {
+    "revision": "e7f7c6e5fd18e0255d11b4fc5d6eeeb5",
+    "url": "/calorie-calculator/static/media/football.e7f7c6e5.jpg"
+  },
+  {
+    "revision": "c2b333b34ca5f4175089a02d83210391",
+    "url": "/calorie-calculator/static/media/jogging.c2b333b3.jpg"
+  },
+  {
+    "revision": "8ed43026c62e9420f15af07dcbd03dd1",
+    "url": "/calorie-calculator/static/media/workout.8ed43026.jpg"
+  }
+]);
